b3c044eae1fd48686e17d46bc2dee51a6e3bb6e0
Toon Ketels <toonketels@A1.local>
2015-02-04 17:49:36 +0100

    Added pagerView

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ ONE
   - plain collection and itemView
 - Benefits
   - auto rerender on paging/sorting/filtering...
+
+
+
+0.2
+
+- Added pagerView
+
++ pagerView acts on same projection, views dont know from one another
++ modelEvents/collectionEvents very descriptive/succint
++ obscura acts just like a collection, can be passed to any view
--- src/js/boot.js
+++ src/js/boot.js
@@ -9,6 +9,7 @@ define([
 	'domain/users',
 
 	'tpl!./ui/tpl/usersItemView.tpl',
+	'tpl!./ui/tpl/paginationView.tpl',
 
 
 
@@ -20,7 +21,8 @@ define([
 	config,
 	UsersCollection,
 
-	usersItemViewTpl
+	usersItemViewTpl,
+	paginationViewTpl
 
 ) {
 
@@ -40,7 +42,7 @@ define([
 	// Create collections
 	
 	var users = new UsersCollection(config.users);
-	var usersProjection = new Projection(users).setPerPage(25);
+	var usersProjection = new Projection(users).setPerPage(5);
 
 
 
@@ -54,10 +56,54 @@ define([
 	var usersView = new Marionette.CollectionView({tagName: 'ul', collection: usersProjection, childView: userItemView });
 
 
+
+	// Pagination view, can paginate any projection
+
+	var PagerView = Marionette.ItemView.extend({
+		initialize     : function(options) {
+			this.model = new Backbone.Model();
+			this.updateState();
+		},
+		tagName        : 'nav',
+		template       : paginationViewTpl,
+		updateState    : function() {
+			this.model.set('isFirst', !this.collection.hasPrevPage());
+			this.model.set('isLast', !this.collection.hasNextPage());
+		},
+		templateHelpers: function() {
+			return {
+				prev: function() {
+					return this.isFirst ? 'disabled' : '';
+				},
+				next: function() {
+					return this.isLast ? 'disabled' : '';
+				},
+			};
+		},
+		modelEvents: {
+			'change': 'render'
+		},
+		collectionEvents: {
+			'paginated:change:page': 'updateState'
+		},
+		events: {
+			'click .prev': 'previous',
+			'click .nxt' : 'next'
+		},
+		previous: function() {
+			this.collection.prevPage();
+		},
+		next    : function() {
+			this.collection.nextPage();
+		}
+	});
+
+
+
 	// Render the views
 
 	regions.main.show(usersView);
-
+	regions.footer.show(new PagerView({collection: usersProjection}));
 
 
 
